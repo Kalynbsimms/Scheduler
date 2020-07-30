a538ae7d0aab1df99f64f4429ce6225bb8a083c0
Kalyn Simms <Kalynbsimms@gmail.com>
2020-07-30 12:16:54 -0700

    code

---
+++ app.js
@@ -0,0 +1,26 @@
+// Display the current date on the page
+$('#currentDay').html(moment().format('dddd, MMMM Do, YYYY'));
+
+
+const currentTime = moment().format('h');
+console.log(currentTime)
+
+let hours = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+let currentIndex = hours.indexOf(parseInt(currentTime))
+
+
+// Determines if times are in the past, present, or future
+for (let i = 0; i < hours.length; i++) {
+    if (i < currentIndex) {
+        $(`#hour-${hours[i]}`).addClass('past')
+    } else if (i > currentIndex) {
+        $(`#hour-${hours[i]}`).addClass('future')
+    } else {
+        $(`#hour-${hours[i]}`).addClass('present')
+    }
+}
+
+// let past = <currentTime
+
+
+// $(selector).siblings(filter)
