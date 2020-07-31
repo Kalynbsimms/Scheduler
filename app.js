fecba1fc244a67039ac101a40374feae29c825b2
Kalyn Simms <Kalynbsimms@gmail.com>
2020-07-31 09:27:51 -0700

    updated local storage

--- app.js
+++ app.js
@@ -36,15 +36,15 @@ $(".saveBtn").click(function () {
 
 // This 
 
-$("#9 .description").val(localStorage.getItem("9"))
-$("#10 .description").val(localStorage.getItem("10"))
-$("#11 .description").val(localStorage.getItem("11"))
-$("#12 .description").val(localStorage.getItem("12"))
-$("#1 .description").val(localStorage.getItem("1"))
-$("#2 .description").val(localStorage.getItem("2"))
-$("#3 .description").val(localStorage.getItem("3"))
-$("#4 .description").val(localStorage.getItem("4"))
-$("#5 .description").val(localStorage.getItem("5"))
+$("#hour-9 .description").val(localStorage.getItem("hour-9"))
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+$("#hour-1 .description").val(localStorage.getItem("hour-1"))
+$("#hour-2 .description").val(localStorage.getItem("hour-2"))
+$("#hour-3 .description").val(localStorage.getItem("hour-3"))
+$("#hour-4 .description").val(localStorage.getItem("hour-4"))
+$("#hour-5 .description").val(localStorage.getItem("hour-5"))
 
 
 
